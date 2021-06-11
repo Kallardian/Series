ca0f85b32b247f4074e94f11b6fa53112c5391f4
Kallardian <paulobertanha03@gmail.com>
2021-06-11 08:14:47 -0300

    Fazendo input - Controlled Components

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -3,17 +3,43 @@ import { View, Text, StyleSheet, TextInput } from "react-native";
 
 import FormRow from "../components/FormRow";
 export default class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mail: "",
+      password: "",
+    };
+  }
+  onChangeMail(value) {
+    this.setState({
+      mail: value,
+    });
+  }
+  onChangePassword(value) {
+    this.setState({
+      password: value,
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <FormRow>
-          <TextInput style={styles.input} placeholder="exemplo@mail.com" />
+          <TextInput
+            style={styles.input}
+            placeholder="exemplo@mail.com"
+            value={this.state.mail}
+            onChangeText={(value) => this.onChangeMail(value)}
+          />
         </FormRow>
         <FormRow>
           <TextInput
             style={styles.input}
             placeholder="******"
             secureTextEntry
+            value={this.state.password}
+            onChangeText={(value) => this.onChangePassword(value)}
           />
         </FormRow>
       </View>
